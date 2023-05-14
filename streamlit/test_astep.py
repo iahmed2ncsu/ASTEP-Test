208db2835d20f92ea1a113d33854c709fa52a052
iahmed2ncsu <50496076+iahmed2ncsu@users.noreply.github.com>
2023-05-14 16:17:34 -0400

    Update test_astep.py

--- streamlit/test_astep.py
+++ streamlit/test_astep.py
@@ -22,11 +22,11 @@ from numpy import genfromtxt
 st.set_page_config(page_title=None, page_icon=None, 
                    layout="wide", 
                    initial_sidebar_state="auto", menu_items=None)
-os.chdir("./streamlit")
+#os.chdir("./streamlit")
 ### Define Tab Names and Numbers
 tab1, tab2,tab3, tab4 = st.tabs(["Inputs 1-3", "Inputs 4-7","Run Economic Assessment","Results"])
 
-test_df = pd.read_csv("../coal_petrolium_red.csv", header=0,index_col = 0)
+test_df = pd.read_csv("coal_petrolium_red.csv", header=0,index_col = 0)
 with tab1:
     st.write(os.getcwd())
     st.write(test_df)
